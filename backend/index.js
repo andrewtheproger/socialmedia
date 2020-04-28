79ad6f5b5c57ce309fde654be5468dc6e663f027
Andrew Bokhonov <bokhonov7@gmail.com>
2020-04-28 18:27:23 +0400

    testswidget

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,25 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-app.get("/", function(req, res){
-    res.json({works: "true"})
-})
-app.listen(80)
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Test = require("models/test")
+app.use(cors());
+
+async function start() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://andrewtheproger:OjRozl6riBCuNjoV@cluster0-oujqx.mongodb.net/test?retryWrites=true&w=majority",
+      { useNewUrlParser: true }
+    );
+    app.get("/", function (req, res) {
+      res.json({ works: "true" });
+    });
+    app.get("/", function (req, res) {
+      res.json({ works: "true" });
+    });
+    app.listen(80);
+  } catch (e) {
+    console.log(e);
+  }
+}
+start();
---
+++ frontend/src/components/Testswidget.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="lesson in timetable" :key="lesson">
+        {{
+          lesson + ": " + !rusToEng.isArray()
+            ? !rusToEng(lesson)
+              ? tests[rusToEng(lesson)]
+              : ""
+            : `Первая группа: ${
+                tests[rusToEng(lesson)[0]]
+              }, Вторая группа: ${tests[rusToEng(lesson)[1]]}`
+        }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "Homeworkwidget",
+  props: ["timetable", "tests"],
+  methods: {
+    rusToEng(subj) {
+      switch (subj) {
+        case "Русский язык":
+          return "russian";
+        case "Литература":
+          return "literature";
+        case "Английский язык":
+          return ["englishone", "englishtwo"];
+        case "История":
+          return "history";
+        case "Обществозание":
+          return "sociology";
+        case "География":
+          return "geography";
+        case "Биология":
+          return "biology";
+        case "Химия":
+          return "chemistry";
+        case "Алгебра":
+          return "algebra";
+        case "Геометрия":
+          return "geometry";
+        case "Физика":
+          return "physics";
+        case "Информатика":
+          return ["computerscienceone", "computersciencetwo"];
+        case "Технология":
+          return "technology";
+        case "Музыка":
+          return "music";
+        case "ИЗО":
+          return "art";
+        case "Физкультура":
+          return "pe";
+        default:
+          return false;
+      }
+    },
+  },
+};
+</script>
+<style lang="scss"></style>
